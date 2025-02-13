459dba75b067443fccb3178ebc8c57f61727c1a9
josemicode <jmiguelojedagutierrez@gmail.com>
2025-02-13 13:34:45 +0100

    upd estructura

--- practica1/ej1/propiedad.py
+++ practica1/ej1/propiedad.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-import reglas
+#import reglas
 class Propiedad:
     def __init__(self, identificador, coste_noche):
         self.id = identificador
@@ -13,9 +13,15 @@ class Propiedad:
     
     def precio(self, fecha_inicio, fecha_fin):
         precio = (fecha_fin - fecha_inicio).days * self.coste_noche
-        for regla in reglas:
+        for regla in self.reglas:
             precio += regla.aplicar()
         return precio
+    
+    #TODO: handling de reservas
+    #* Deberia buscar coincidencias en el rango de dias introducido con los de los registros
+    #* Puede devolver un bool para confirmar
+    def reserva(self):
+        pass
 
     #? Handling de fechas...
     #* Finalmente, se esperaran objetos datetime como parametros; formatear en el main
--- practica1/ej1/usuario.py
+++ practica1/ej1/usuario.py
@@ -1,6 +1,16 @@
+from propiedad import Propiedad
 class Usuario:
     def __init__(self, identificador):
         self.id = identificador
         self.propiedades = []
+    
+    def registrarPropiedad(self, coste_noche):
+        id_generado = self.id + str(len(self.propiedades))
+        nueva_propiedad = Propiedad(id_generado, coste_noche)
+        self.propiedades.append(nueva_propiedad)
+        pass
+
+    def alquilarPropiedad(self):
+        pass
 
 #* Asumimos la capacidad de multipropiedades
