fa0876f80c49eebf83221e7d0a65ae83b12692ef
josemicode <jmiguelojedagutierrez@gmail.com>
2025-02-13 13:22:34 +0100

    calcular precio

--- practica1/ej1/propiedad.py
+++ practica1/ej1/propiedad.py
@@ -4,10 +4,18 @@ class Propiedad:
     def __init__(self, identificador, coste_noche):
         self.id = identificador
         self.coste_noche = coste_noche
-        self.reservada = False
+        self.reglas = []
+        #TODO: self.reservas = []
     
-    def precio(self):
-        pass
+    def nuevaRegla(self, regla):
+        self.reglas.append(regla)
+        self.reglas.sort(key=lambda r: r.prioridad)
+    
+    def precio(self, fecha_inicio, fecha_fin):
+        precio = (fecha_fin - fecha_inicio).days * self.coste_noche
+        for regla in reglas:
+            precio += regla.aplicar()
+        return precio
 
     #? Handling de fechas...
     #* Finalmente, se esperaran objetos datetime como parametros; formatear en el main
