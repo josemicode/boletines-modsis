5ce7255375c6c22bdbd10b59d26aaa1d46dbaaf1
josemicode <jmiguelojedagutierrez@gmail.com>
2025-03-13 15:54:11 +0100

    added productor.py

---
+++ practica2/productor.py
@@ -0,0 +1,9 @@
+class Productor:
+    def __init__(self, nombre, apellidos, NIF, direccion, telefono, email):
+        self.nombre = nombre
+        self.apellidos = apellidos
+        self.nif = NIF
+        self.direccion = direccion
+        self.telefono = telefono
+        self.email = email
+        
