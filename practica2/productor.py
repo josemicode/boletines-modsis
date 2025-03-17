b44d101413699955dd2c19b27e57d4b2fde3871f
PCRDO\walls <wallstreetdoggo@gmail.com>
2025-03-17 19:38:46 +0100

    getters productor

--- practica2/productor.py
+++ practica2/productor.py
@@ -1,9 +1,27 @@
 class Productor:
     def __init__(self, nombre, apellidos, NIF, direccion, telefono, email):
-        self.nombre = nombre
-        self.apellidos = apellidos
-        self.nif = NIF
-        self.direccion = direccion
-        self.telefono = telefono
-        self.email = email
-        
+        self._nombre = nombre
+        self._apellidos = apellidos
+        self._nif = NIF
+        self._direccion = direccion
+        self._telefono = telefono
+        self._email = email
+    
+    # Getters
+    def getNombre(self):
+        return self._nombre
+    
+    def getApellidos(self):
+        return self._apellidos
+    
+    def getNIF(self):
+        return self._nif
+    
+    def getDireccion(self):
+        return self._direccion
+    
+    def getTelefono(self):
+        return self._telefono
+    
+    def getEmail(self):
+        return self._email
