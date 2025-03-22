2036cb32b4fa5001793be80255ae7bcc4d0b6a34
PCRDO\walls <wallstreetdoggo@gmail.com>
2025-03-22 19:54:21 +0100

    atributos de productos

--- practica2/enumeraciones.py
+++ practica2/enumeraciones.py
@@ -11,3 +11,19 @@ class NivelFrutado(Enum):
     ALTO = "Nivel Frutado Alto"
     MEDIO = "Nivel Frutado Medio"
     BAJO = "Nivel Frutado Bajo"
+
+class UniformidadColor(Enum):
+    ALTO = "Uniformidad de Color Alta"
+    MEDIO = "Uniformidad de Color Media"
+    BAJO = "Uniformidad de Color Baja"
+
+class PerfilSabor(Enum):
+    AMARGO = "Perfil de Sabor Amargo"
+    SALADO = "Perfil de Sabor Salado"
+    ACIDO = "Perfil de Sabor Acido"
+
+class ProcessoCurado(Enum):
+    NATURAL = "Proceso de Curado Natural"
+    SALMUERA = "Proceso de Curado en Salmuera"
+    SOSA = "Proceso de Curado en Sosa Caustica"
+    FERMETACION = "Proceso de Curado por Fermentacion"
--- practica2/productos.py
+++ practica2/productos.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-# from enumeraciones import MetodoExtraccion, NivelFrutado
+# from enumeraciones import MetodoExtraccion, NivelFrutado, UniformidadColor, PerfilSabor, ProcesoCurado
 import random
 
 class Producto(ABC):
@@ -29,6 +29,7 @@ class Aceite(Producto):
         self.defectos_sensoriales = [] # List[DefectoSensorial]
         self.perfil_frutado = None # str
         self.nivel_frutado = None # NivelFrutado
+        self.puntaje = None # float
     
     # Setters
     def setMetodoExtraccion(self, metodo):
@@ -55,9 +56,97 @@ class Aceite(Producto):
     def aceptarAnalizador(self, analizador):
         return analizador.analizarAceite(self)
 
+    # Getters
+    def getMetodoExtraccion(self):
+        return self.metodo_extraccion
+
+    def getAcidez(self):
+        return self.acidez
+
+    def getCantidadPolifenoles(self):
+        return self.cantidad_polifenoles
+
+    def getColor(self):
+        return self.color
+
+    def getDefectosSensoriales(self):
+        return self.defectos_sensoriales
+
+    def getPerfilFrutado(self):
+        return self.perfil_frutado
+
+    def getNivelFrutado(self):
+        return self.nivel_frutado
+
+    def getPuntaje(self):
+        return self.puntaje
+
+
 class Oliva(Producto):
     def __init__(self):
         super().__init__()
+        self.uniformidad_color = None # UniformidadColor
+        self.tamano_promedio = None # float
+        self.desvio_tamano = None # float
+        self.perfil_sabor = None # PerfilSabor
+        self.proceso_curado = None # ProcesoCurado
+        self.contenido_sal = None # float
+        self.porcentaje_defectos_visuales = None # float
+        self.ph = None # float
+        self.puntaje = None # float
+
+    #Getters
+    def getUniformidadColor(self):
+        return self.uniformidad_color
+
+    def getTamanoPromedio(self):
+        return self.tamano_promedio
+
+    def getDesvioTamano(self):
+        return self.desvio_tamano
+
+    def getPerfilSabor(self):
+        return self.perfil_sabor
+
+    def getProcesoCurado(self):
+        return self.proceso_curado
+
+    def getContenidoSal(self):
+        return self.contenido_sal
+
+    def getPorcentajeDefectosVisuales(self):
+        return self.porcentaje_defectos_visuales
+
+    def getPh(self):
+        return self.ph
+
+    def getPuntaje(self):
+        return self.puntaje
+    
+    # Setters
+    def setUniformidadColor(self, uniformidad):
+        self.uniformidad_color = uniformidad
+    
+    def setTamanoPromedio(self, tamano):
+        self.tamano_promedio = tamano
+
+    def setDesvioTamano(self, desvio):
+        self.desvio_tamano = desvio
+
+    def setPerfilSabor(self, perfil):
+        self.perfil_sabor = perfil
+    
+    def setProcesoCurado(self, proceso):
+        self.proceso_curado = proceso
+
+    def setContenidoSal(self, contenido):
+        self.contenido_sal = contenido
+
+    def setPorcentajeDefectosVisuales(self, porcentaje):
+        self.porcentaje_defectos_visuales = porcentaje
+    
+    def setPh(self, ph):
+        self.ph = ph
     
     def aceptarAnalizador(self, analizador):
         return analizador.analizarOliva(self)
