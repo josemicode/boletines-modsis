5594e722fe39731ad39806dde259b202d462e07f
PCRDO\walls <wallstreetdoggo@gmail.com>
2025-03-23 19:55:43 +0100

    deleted resultados

--- practica2/atributo_resultado.py
+++
@@ -1,9 +0,0 @@
-class AtributoResultado:
-    def __init__(self, nombre, valor):
-        self.nombre = nombre
-        self.valor = valor
-
-    def __str__(self):
-        return f"{self.nombre} ({self.valor})"
-
-#! Clase Posiblemente obsoleta
